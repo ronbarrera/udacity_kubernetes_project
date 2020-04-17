978616b78284fecea9b7bb552b4870fcbff84c93
ronaldbarrera <ralexander.barrera@gmail.com>
2020-04-16 21:42:24 -0700

    test commit to check codepipeline

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ def require_jwt(function):
 
 @APP.route('/', methods=['POST', 'GET'])
 def health():
-    return jsonify("Healthy")
+    return jsonify("Healthy x2")
 
 
 @APP.route('/auth', methods=['POST'])
