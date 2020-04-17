a3f432763d7be8b0f2a0a2d6b38419de8b962d4e
ronaldbarrera <ralexander.barrera@gmail.com>
2020-04-16 22:22:38 -0700

    added test to prebuild

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ def require_jwt(function):
 
 @APP.route('/', methods=['POST', 'GET'])
 def health():
-    return jsonify("Healthy x2")
+    return jsonify("Healthy")
 
 
 @APP.route('/auth', methods=['POST'])
